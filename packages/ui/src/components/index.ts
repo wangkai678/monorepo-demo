4c022049fbbe1ff27e20061ce0490be9cf01536d
wangkai678 <wangkai_678@163.com>
2025-05-06 14:56:00 +0800

    feat: 初始化

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,4 @@
+// import Layout from "./layout/index.vue"
+// export {Layout}
+export { default as Layout } from "./layout/index.vue"
+
---
+++ packages/ui/src/components/layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="color: cadetblue;font-size: 100px;">
+      测试组件
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  // 可以添加导航逻辑、inject、pinia 等
+  </script>
+  
+  <style scoped>
+
+  </style>
